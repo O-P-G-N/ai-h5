d8277d2c2eeb373936cd3e79f913443403bfbdbe
oujie <1090873491@qq.com>
2023-10-26 15:33:43 +0800

    个人中心头部

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,11 +1,44 @@
 <template>
-	<view>
-		<view>user</view>
+	<view class="user_index">
+		<view class="user_head">
+			<view class="user_head_left">
+				<view class="name">Ks</view>
+				<view class="account_level">
+					
+				</view>
+			</view>
+			<image class="user_head_right" :mode="item.mode" :src="src"
+			                        @error="imageError"></image>
+		</view>
 	</view>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.user_index {
+		width: 100vw;
+		height: 100vh;
+		padding: 16px;
+
+		.user_head {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			height: 59px;
+
+			.user_head_left {
+				.name {
+					font-size: 26px;
+					color: rgb(0, 0, 0);
+				}
+			}
+			.user_head_right{
+				
+			}
+		}
+
+		
+	}
 </style>
