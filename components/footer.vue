eda54d5433ed76da5acc0e01d64debe1a25085c3
向 <1013876478@qq.com>
2023-10-26 15:11:42 +0800

    fix:页面创建

---
+++ components/footer.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="footerMain">
+		<view class="footer_every">
+			<image class="icon" src="~@/static/tabbar/one.png"></image>
+		</view>
+		<view class="footer_every">
+			<image class="icon" src="~@/static/tabbar/two.png"></image>
+		</view>
+		<view class="footer_every" style="width: 28%;">
+			<image class="icon" src="~@/static/tabbar/one.png"></image>
+		</view>
+		<view class="footer_every">
+			<image class="icon" src="~@/static/tabbar/four.png"></image>
+		</view>
+		<view class="footer_every">
+			<image class="icon" src="~@/static/tabbar/five.png"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+	
+			}
+		},
+		created() {
+		},
+		methods: {
+	
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.footerMain {
+		position: fixed;
+		z-index: 99;
+		height: 124rpx;
+		bottom: 42rpx;
+		left: 15rpx;
+		right: 0;
+		width: 720rpx;
+		border-radius: 32px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background: #ededed;
+		.footer_every{
+			    width: 18%;
+				height: 100%;
+			    display: flex;
+				flex-direction: column;
+				justify-content: center;
+			    align-items: center;
+			    font-size: 17px;
+			    position: relative;
+				.icon{
+					width: 84rpx;
+					height: 84rpx;
+				}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,18 +1,25 @@
 <template>
 	<view class="container">
 		<view>index</view>
-
+		<view class="mian"></view>
 		<u-icon name="photo" color="#2979ff" size="28"></u-icon>
 		<u-button type="success" :ripple="true">test</u-button>
+		<Footer></Footer>
 	</view>
 </template>
 
+
 <script>
 	export default {
+		components:{
+			Footer:()=>import('@/components/footer.vue')
+		},
 		data() {
 			return {
-				
+
 			}
+		},
+		created() {
 		},
 		methods: {
 
@@ -22,9 +29,16 @@
 
 <style lang="scss">
 	@import "@/uni_modules/uview-ui/index.scss";
+
 	.container {
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
+	}
+
+	.mian {
+		width: 750rpx;
+		height: 300rpx;
+		background-color: red;
 	}
 </style>
