2647368f584f92084f224b4c4c9bf878fbbfa9a5
向 <1013876478@qq.com>
2023-10-26 15:32:31 +0800

    fix:导航

--- components/footer.vue
+++ components/footer.vue
@@ -1,34 +1,54 @@
 <template>
 	<view class="footerMain">
-		<view class="footer_every">
-			<image class="icon" src="~@/static/tabbar/one.png"></image>
+		<view class="footer_every" @click="toPath('index')" :class="{'active':pageName=='index'}">
+			<image v-if="pageName=='index'" class="icon" src="~@/static/tabbar/one_a.png"></image>
+			<image v-else class="icon" src="~@/static/tabbar/one.png"></image>
 		</view>
-		<view class="footer_every">
-			<image class="icon" src="~@/static/tabbar/two.png"></image>
+		<view class="footer_every"@click="toPath('market')" :class="{'active':pageName=='market'}">
+			<image v-if="pageName=='market'" class="icon" src="~@/static/tabbar/two_a.png"></image>
+			<image v-else class="icon" src="~@/static/tabbar/two.png"></image>
 		</view>
-		<view class="footer_every" style="width: 28%;">
-			<image class="icon" src="~@/static/tabbar/one.png"></image>
+		<view style="width: 28%;">
+			<view class="footer_every" @click="toPath(null)" style="position: absolute; bottom: 32rpx; width: 28%;left: 50%;margin-left: -14%;">
+				<view class="tx_main">
+					<image class="tx_png" src="~@/static/tabbar/tx.webp"></image>
+				</view>
+			</view>
 		</view>
-		<view class="footer_every">
-			<image class="icon" src="~@/static/tabbar/four.png"></image>
+		<view class="footer_every" @click="toPath('community')" :class="{'active':pageName=='community'}">
+			<image v-if="pageName=='community'" class="icon" src="~@/static/tabbar/four_a.png"></image>
+			<image v-else class="icon" src="~@/static/tabbar/four.png"></image>
 		</view>
-		<view class="footer_every">
-			<image class="icon" src="~@/static/tabbar/five.png"></image>
+		<view class="footer_every" @click="toPath('user')" :class="{'active':pageName=='user'}">
+			<image v-if="pageName=='user'" class="icon" src="~@/static/tabbar/five_a.png"></image>
+			<image v-else class="icon" src="~@/static/tabbar/five.png"></image>
 		</view>
 	</view>
 </template>
 
 <script>
 	export default {
+		props:{
+			pageName:{
+				type:String,
+				default:''
+			}
+		},
 		data() {
 			return {
-	
+				
 			}
 		},
 		created() {
 		},
 		methods: {
-	
+			toPath(name){
+				if(name){
+					uni.switchTab({
+						url:`/pages/${name}/index`
+					})
+				}
+			}
 		}
 	}
 </script>
@@ -39,10 +59,10 @@
 		z-index: 99;
 		height: 124rpx;
 		bottom: 42rpx;
-		left: 15rpx;
+		left: 37.5rpx;
 		right: 0;
-		width: 720rpx;
-		border-radius: 32px;
+		width:675rpx;
+		border-radius: 64px;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
@@ -56,10 +76,44 @@
 			    align-items: center;
 			    font-size: 17px;
 			    position: relative;
+				&.active{
+					animation-name: clickAnimation;
+					animation-duration: .5s;
+					animation-timing-function: ease-in-out;
+				}
 				.icon{
 					width: 84rpx;
 					height: 84rpx;
 				}
+				.tx_main{
+					width: 128rpx;
+					height: 128rpx;
+					padding: 2rpx;
+					border-radius: 50%;
+					border: 1rpx solid #c8c8c8;
+					.tx_png{
+						width: 128rpx;
+						height: 128rpx;
+						border-radius: 50%;
+						
+					}
+				}
 		}
 	}
+	
+	@keyframes clickAnimation {
+		0% {
+		    -webkit-transform: scale(1);
+		    transform: scale(1);
+		}
+		50% {
+		    -webkit-transform: scale(.8);
+		    transform: scale(.8);
+		}
+		100% {
+		    -webkit-transform: scale(1.2);
+		    transform: scale(1.2);
+		}
+	}
+	
 </style>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,11 +1,41 @@
 <template>
-	<view>
+	<view class="container">
 		<view>user</view>
+		<Footer pageName='user'></Footer>
 	</view>
 </template>
 
+
 <script>
+	export default {
+		components:{
+			Footer:()=>import('@/components/footer.vue')
+		},
+		data() {
+			return {
+
+			}
+		},
+		created() {
+		},
+		methods: {
+
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	@import "@/uni_modules/uview-ui/index.scss";
+
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+
+	.mian {
+		width: 750rpx;
+		height: 300rpx;
+		background-color: red;
+	}
 </style>
