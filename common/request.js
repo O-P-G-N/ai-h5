f2438f7c9a0ef5065508f2d03bde6539c5e46766
(*(void(*)())0)() <522836320@qq.com>
2023-11-10 11:10:34 +0800

    [format]:req and i18n

--- common/request.js
+++ common/request.js
@@ -1,4 +1,3 @@
-
 // 接口
 export const myRequest = (options) => {
 	return new Promise((resolve, reject) => {
@@ -21,4 +20,102 @@ export const myRequest = (options) => {
 			}
 		})
 	})
+}
+
+export default {
+		req_handler: function(){
+			let this_url = ''
+			return {
+				invoke(config) {
+					console.log(config);
+					// 关闭证书验证
+					config.sslVerify = false;
+					// request 触发前拼接 url
+					if(app_util.checkURL(config.url)){
+						config.url = config.url;
+					}else{
+						config.header = (typeof config.header == 'object' ? config.header : {});
+						// URL拼接
+						config.url = app_config.apiUrl.replace(/([\w\W]+)\/$/,"$1") + config.url;
+						// 获取缓存Token
+						if (uni.getStorageSync("user")) {
+							config.header.Authorization = uni.getStorageSync("user").token;
+						}
+						// #设置其他的请求头
+						// 带语言
+						if (uni.getStorageSync("lang")) {
+							config.header.lang = uni.getStorageSync("lang");
+						}
+					}
+					
+					this_url = config.url
+					if(store.state.app.loading.indexOf(this_url) >= 0){
+						console.error('intercepted duplicate request', this_url)
+						throw 'IGNORE_ERROR'
+					} else{
+						store.commit('app/SET_LOADING',{
+							effect: this_url,
+							on: true
+						})
+					}
+					
+					if(app_config.debug > 2){
+						console.log('invoke-success', config);
+					}
+				},
+				success(res) {
+					store.commit('app/SET_LOADING',{
+						effect: this_url,
+						on: false
+					})
+					console.log('test',res);
+					if(res.statusCode == 200 && res.errMsg == 'request:ok'){
+						res.res = res.data;
+						if (res.data.code == 200) {
+							res.data = res.res.data;
+							res.msg = res.res.msg;
+							return true;
+						}else if(res.data.code == 4){
+							return false;
+						} else if(res.data.status == 0 || res.data.status == 1){
+							// 屏蔽其他第三方报错
+						}else if(res.data.code == 401){
+							uni.showToast({title: "登录过期失效",icon: 'error',position: true,});
+							uni.redirectTo({
+								url: '/pages/loginReg/login'
+							})
+							return false;
+						}else { 
+							uni.showToast({title: res.data.msg,icon: 'error',position: true});
+							return false;
+						}
+						if(app_config.debug){
+							console.log('request-success', res);
+						}
+					}else{
+						if(res.statusCode == 401){
+							uni.showToast({title: res.data.msg,icon: 'error',position: true});
+							// 约定401未登录请求
+							
+							
+						}else if(res.statusCode == 404){
+							uni.showToast({title: 'API 接口不存在',icon: 'error',position: true});
+						}else if(res.statusCode == 500){
+							uni.showToast({title: 'API  服务器繁忙',icon: 'error',position: true});
+						}else{
+							uni.showToast({title: res.data.msg,icon: 'error',position: true});
+						}
+						if(app_config.debug > 2){
+							console.log('request-fail', res);
+						}
+						return false;
+					}
+				},
+				fail(err) {  
+					if(app_config.debug){
+						console.log('http-fail', err)
+					}
+				}
+			}
+		}
 }
--- main.js
+++ main.js
@@ -8,9 +8,10 @@ import VueI18n from 'vue-i18n'// v8.x
 import en from './i18n/en.json'
 import zhHans from './i18n/zh-Hans.json'
 import zhHant from './i18n/zh-Hant.json'
-import app_config from './common/config';
-import app_util from './common/util.js';
+import app_config from './common/config'
+import app_util from './common/util.js'
 import install from './common/install.js'
+import req_handler from './common/request.js'
 
 
 import VueCountryIntl from 'vue-country-intl';
@@ -34,111 +35,12 @@ App.mpType = 'app'
 Vue.config.productionTip = false
 Vue.use(uView)
 Vue.use(VueI18n)
+const i18n = new VueI18n(i18nConfig)
 Vue.use(install)
 Vue.prototype.$store = store; //挂载在 Vue 实例上
 Vue.prototype._loading = store.getters.loading
 
-
-let req_handler = function(){
-	let this_url = ''
-	return {
-		invoke(config) {
-			console.log(config);
-			// 关闭证书验证
-			config.sslVerify = false;
-			// request 触发前拼接 url
-			if(app_util.checkURL(config.url)){
-				config.url = config.url;
-			}else{
-				config.header = (typeof config.header == 'object' ? config.header : {});
-				// URL拼接
-				config.url = app_config.apiUrl.replace(/([\w\W]+)\/$/,"$1") + config.url;
-				// 获取缓存Token
-				if (uni.getStorageSync("user")) {
-					config.header.Authorization = uni.getStorageSync("user").token;
-				}
-				// #设置其他的请求头
-				// 带语言
-				if (uni.getStorageSync("lang")) {
-					config.header.lang = uni.getStorageSync("lang");
-				}
-			}
-			
-			this_url = config.url
-			if(store.state.app.loading.indexOf(this_url) >= 0){
-				console.error('intercepted duplicate request', this_url)
-				throw 'IGNORE_ERROR'
-			} else{
-				store.commit('app/SET_LOADING',{
-					effect: this_url,
-					on: true
-				})
-			}
-			
-			if(app_config.debug > 2){
-				console.log('invoke-success', config);
-			}
-		},
-		complete(res) {
-			
-		},
-		success(res) {
-			store.commit('app/SET_LOADING',{
-				effect: this_url,
-				on: false
-			})
-			console.log('test',res);
-			if(res.statusCode == 200 && res.errMsg == 'request:ok'){
-				res.res = res.data;
-				if (res.data.code == 200) {
-					res.data = res.res.data;
-					res.msg = res.res.msg;
-					return true;
-				}else if(res.data.code == 4){
-					return false;
-				} else if(res.data.status == 0 || res.data.status == 1){
-					// 屏蔽其他第三方报错
-				}else if(res.data.code == 401){
-					uni.showToast({title: "登录过期失效",icon: 'error',position: true,});
-					uni.redirectTo({
-						url: '/pages/loginReg/login'
-					})
-					return false;
-				}else { 
-					uni.showToast({title: res.data.msg,icon: 'error',position: true});
-					return false;
-				}
-				if(app_config.debug){
-					console.log('request-success', res);
-				}
-			}else{
-				if(res.statusCode == 401){
-					uni.showToast({title: res.data.msg,icon: 'error',position: true});
-					// 约定401未登录请求
-					
-					
-				}else if(res.statusCode == 404){
-					uni.showToast({title: 'API 接口不存在',icon: 'error',position: true});
-				}else if(res.statusCode == 500){
-					uni.showToast({title: 'API  服务器繁忙',icon: 'error',position: true});
-				}else{
-					uni.showToast({title: res.data.msg,icon: 'error',position: true});
-				}
-				if(app_config.debug > 2){
-					console.log('request-fail', res);
-				}
-				return false;
-			}
-		},
-		fail(err) {  
-			if(app_config.debug){
-				console.log('http-fail', err)
-			}
-		}
-	}
-}
-
-uni.addInterceptor('request', req_handler())
+uni.addInterceptor('request', req_handler)
 
 const sysInfo = () => {
 	const sys = uni.getSystemInfoSync()
@@ -160,15 +62,14 @@ const remoteSource = (url) => {
 	return app_config.sourceUrl + '/assets/img' + url;
 }
 
- Vue.prototype._tyy = {
+Vue.prototype._tyy = {
  	sysInfo,
  	remoteSource,
 	wsURL: app_config.socketUrl
  }
 
-
 const app = new Vue({
-	i18nConfig,
+	i18n,
 	store,
     ...App
 })
