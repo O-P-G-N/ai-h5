8e1dfaee56242074bc0d3b9c25512ba8bc7f7804
a.b5-62@163.com <>
2023-12-22 20:36:37 +0800

    1

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 
-cd /c/work/tyy-ai-h5/
+cd /Users/xiaokangy/work/tyy-ai-h5/
 git checkout -f
 git checkout -f
 git pull
@@ -7,7 +7,7 @@ git pull
 #rm -rf unpackage
 cat common/1.js>common/config.js
 cli publish --platform h5 --project tyy-ai-h5
-cd /c/work/tyy-ai-h5/unpackage/dist/build/
+cd /Users/xiaokangy/work/tyy-ai-h5/unpackage/dist/build/
 f=97465e7b
 cd h5
 sed 's/${f}//g' index.${f}.css > index.css
@@ -16,5 +16,5 @@ tar czvf  h5.tar.gz ./h5
 ssh root@49.0.201.46 "rm -rf /www/wwwroot/www\.aigptstar\.com/h5*"
 scp h5.tar.gz root@49.0.201.46:/www/wwwroot/www\.aigptstar\.com/
 ssh root@49.0.201.46 "cd /www/wwwroot/www\.aigptstar\.com/ && tar -zxvf h5\.tar\.gz"
-cd /c/work/tyy-ai-h5/
+cd /Users/xiaokangy/work/tyy-ai-h5/
 git checkout -f
